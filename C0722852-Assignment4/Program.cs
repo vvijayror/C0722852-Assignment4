f0244768f59278e97f45778da35beb76a5450f99
visualstudio <visualstudio@122-PC23>
2019-04-10 10:05:38 -0400

    Section A

--- C0722852-Assignment4/Program.cs
+++ C0722852-Assignment4/Program.cs
@@ -3,13 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
+using System.IO;
 
 namespace C0722852_Assignment4
 {
     class Program
     {
+
+        ArrayList Beowulf;
         static void Main(string[] args)
         {
+            Program p = new Program();
+            p.Beowulf = new ArrayList();
+
         }
+        public void Run() { this.ReadTextFiles(); }
+
+        public void ReadTextFiles()
+        {
+            using (StreamReader file = new StreamReader("U:/Users/722852/beowulf.txt"))
+            {
+                int counter = 0;
+                string ln;
+
+                while ((ln = file.ReadLine()) != null)
+                    {
+                    Console.WriteLine(ln);
+                    Beowulf.Add(ln);
+                }
+                file.Close();
+                Console.WriteLine($"File has {counter} lines.");
+            }
+        }
+      
     }
 }
